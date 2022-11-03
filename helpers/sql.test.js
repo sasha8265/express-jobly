3461a7477d7d2379131344b89700e8ff3490aa73
Sasha Czerniawski <sashacandoit@gmail.com>
2022-11-03 18:55:19 -0400

    added second test for sqlForPartialUpdate function

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,14 +1,24 @@
-const jwt = require("jsonwebtoken");
 const { sqlForPartialUpdate } = require("./sql");
 
 describe("sqlForPartialUpdate", function () {
-    test("works: data", function () {
+    test("works: update 1 item", function () {
         const dataToUpdate = { val1: 'newVal1'};
-        const jsToSql = { val1: 'val1', v2: 'val2' };
+        const jsToSql = { val1: 'val1', val2: 'val2' };
         const result = sqlForPartialUpdate(dataToUpdate, jsToSql)
         expect(result).toEqual({
             setCols: "\"val1\"=$1",
             values: ["newVal1"]
+        })
+    })
+
+    test("works: update 2 items", function () {
+        const dataToUpdate = { val1: 'newVal1', val2: 'newVal2' };
+        const jsToSql = { val1: 'val1' };
+        const result = sqlForPartialUpdate(dataToUpdate, jsToSql)
+        console.log(result)
+        expect(result).toEqual({
+            setCols: "\"val1\"=$1, \"val2\"=$2",
+            values: ["newVal1", "newVal2"]
         })
     })
 })
