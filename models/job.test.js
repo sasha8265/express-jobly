d297c67ad53c493b8eb4bcd8750e902bc3dfa9cc
Sasha Czerniawski <sashacandoit@gmail.com>
2022-11-09 06:28:52 -0500

    delete method done and tests pass

--- models/job.test.js
+++ models/job.test.js
@@ -213,3 +213,24 @@ describe("update", function () {
         }
     });
 });
+
+
+/************************************** delete */
+
+describe("remove", function () {
+    test("works", async function () {
+        await Job.remove(testJobIds[0]);
+        const res = await db.query(
+            "SELECT id FROM jobs WHERE id=$1", [testJobIds[0]]);
+        expect(res.rows.length).toEqual(0);
+    });
+
+    test("not found if no such job", async function () {
+        try {
+            await Job.remove(0);
+            fail();
+        } catch (err) {
+            expect(err instanceof NotFoundError).toBeTruthy();
+        }
+    });
+});
